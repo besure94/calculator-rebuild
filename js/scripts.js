45462f704aa2bf27a140c755c1f9d5e3c016676d
Brian Scherner <brianscherner@gmail.com>
2023-05-08 15:05:17 -0700

    Add handleCalculation event and define number1, number2 and operator as variables.

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,10 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
 	return number1 / number2;
 }
+
+function handleCalculation(event) {
+	event.preventDefault();
+	const number1 = parseInt(document.querySelector("input#input1").value);
+	const number2 = parseInt(document.querySelector("input#input2").value);
+	const operator = document.querySelector("input[name='operator']:checked").value;
+}
